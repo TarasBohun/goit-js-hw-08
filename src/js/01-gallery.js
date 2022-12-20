90a05a40515cbb76d18518dbed398c12f507711d
Taras <tarasbohun07@gmail.com>
2022-12-20 21:39:30 +0200

    done task-2

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -6,7 +6,7 @@ import 'simplelightbox/dist/simple-lightbox.min.css';
 import { galleryItems } from './gallery-items';
 // Change code below this line
 // console.log(SimpleLightbox);
-console.log(galleryItems);
+// console.log(galleryItems);
 
 const gallery = document.querySelector('.gallery');
 gallery.insertAdjacentHTML('beforeend', renderGalleryItems(galleryItems));
--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,33 @@
+var throttle = require('lodash.throttle');
 
+import Player from '@vimeo/player';
+
+const iframe = document.querySelector('#vimeo-player');
+const player = new Player(iframe);
+const LOCALSTORAGE_KEY = 'videoplayer-current-time';
+
+resumeTime();
+
+player.on(
+  'timeupdate',
+  throttle(data => {
+    localStorage.setItem(LOCALSTORAGE_KEY, data.seconds);
+  }, 1000)
+);
+
+function resumeTime() {
+  let setTime = localStorage.getItem(LOCALSTORAGE_KEY);
+  if (setTime)
+    player
+      .setCurrentTime(setTime)
+      .then(function (seconds) {})
+      .catch(function (error) {
+        switch (error.name) {
+          case 'RangeError':
+            break;
+          default:
+            break;
+        }
+      });
+  console.log(setTime);
+}
