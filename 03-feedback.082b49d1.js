b544d51e207dbf9dc22faad009f52c0218d86ad6
TarasBohun <102221634+TarasBohun@users.noreply.github.com>
2022-12-21 13:02:46 +0000

    Deploying to gh-pages from @ TarasBohun/goit-js-hw-08@eb4931a00a44eebbe237f43ee4ef8ae912246d89 🚀

---
+++ 03-feedback.082b49d1.js
@@ -0,0 +1,2 @@
+!function(){document.querySelector("form");console.log("form");console.log(11)}();
+//# sourceMappingURL=03-feedback.082b49d1.js.map
---
+++ 03-feedback.25e5f5e1.js
@@ -0,0 +1,2 @@
+document.querySelector("form");console.log("form");console.log(11);
+//# sourceMappingURL=03-feedback.25e5f5e1.js.map
